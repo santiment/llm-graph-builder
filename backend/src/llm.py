da7a26683648801c7774aae2a248332ded32f87a
Bulat <lb6557@gmail.com>
2024-08-20 09:45:29 +0300

    feat(backend): add COT to document processing
    <!-- ps-id: 2944211c-0a48-4f2b-b635-94c0ccc4332c -->

--- backend/src/llm.py
+++ backend/src/llm.py
@@ -1,6 +1,14 @@
+import asyncio
 import logging
+from typing import Optional, List, Dict, Tuple, Any, Type, Union
+
 from langchain.docstore.document import Document
 import os
+
+from langchain_core.messages import SystemMessage
+from langchain_core.output_parsers import JsonOutputParser
+from langchain_core.prompts import ChatPromptTemplate, PromptTemplate, HumanMessagePromptTemplate
+from langchain_experimental.graph_transformers.llm import UnstructuredRelation, _Graph, optional_enum_field
 from langchain_openai import ChatOpenAI, AzureChatOpenAI
 from langchain_google_vertexai import ChatVertexAI
 from langchain_groq import ChatGroq
@@ -15,6 +23,8 @@ from langchain_aws import ChatBedrock
 from langchain_community.chat_models import ChatOllama
 import boto3
 import google.auth
+from pydantic import BaseModel, Field
+
 from src.shared.constants import MODEL_VERSIONS
 
 
@@ -129,9 +139,246 @@ def get_combined_chunks(chunkId_chunkDoc_list):
         )
     return combined_chunk_document_list
 
+class UnstructuredRelationWithThoughts(BaseModel):
+    analysis: str = Field(
+        description=(
+            "A short analysis of entities mentioned in the text."
+            "Some famous people or projects or companies perhaps?"
+            "Think step by step and put your thoughts here."
+        )
+    )
+    head: str = Field(
+        description=(
+            "extracted head entity like Microsoft, Apple, John. "
+            "Must use human-readable unique identifier."
+        )
+    )
+    head_type: str = Field(
+        description="type of the extracted head entity like Person, Company, etc"
+    )
+    relation: str = Field(description="relation between the head and the tail entities")
+    tail: str = Field(
+        description=(
+            "extracted tail entity like Microsoft, Apple, John. "
+            "Must use human-readable unique identifier."
+        )
+    )
+    tail_type: str = Field(
+        description="type of the extracted tail entity like Person, Company, etc"
+    )
+
+system_prompt = (
+    "# Knowledge Graph Instructions for GPT-4\n"
+    "## 1. Overview\n"
+    "You are a top-tier algorithm designed for extracting information in "
+    "structured formats to build a knowledge graph."
+    "You are created by Santiment company. They provide cryptoanalytics based on onchain and social data"
+    "Most likely you will deal with social posts of users describing their experience with crypto, or some news and rumors"
+    "Try to capture as much information from the text as possible without "
+    "sacrificing accuracy. You can write comments and analysis but within special 'thoughts' field."
+    "For other fields - do not add any information that is not explicitly "
+    "mentioned in the text.\n"
+    "- **Nodes** represent entities and concepts.\n"
+    "- The aim is to achieve simplicity and clarity in the knowledge graph, making it\n"
+    "accessible for a vast audience.\n"
+    "## 2. Labeling Nodes\n"
+    "- **Consistency**: Ensure you use available types for node labels.\n"
+    "Ensure you use basic or elementary types for node labels.\n"
+    "- For example, when you identify an entity representing a person, "
+    "always label it as **'person'**. Avoid using more specific terms "
+    "like 'mathematician' or 'scientist'."
+    "- **Node IDs**: Never utilize integers as node IDs. Node IDs should be "
+    "names or human-readable identifiers found in the text.\n"
+    "- **Relationships** represent connections between entities or concepts.\n"
+    "Ensure consistency and generality in relationship types when constructing "
+    "knowledge graphs. Instead of using specific and momentary types "
+    "such as 'BECAME_PROFESSOR', use more general and timeless relationship types "
+    "like 'PROFESSOR'. Make sure to use general and timeless relationship types!\n"
+    "## 3. Coreference Resolution\n"
+    "- **Maintain Entity Consistency**: When extracting entities, it's vital to "
+    "ensure consistency.\n"
+    'If an entity, such as "John Doe", is mentioned multiple times in the text '
+    'but is referred to by different names or pronouns (e.g., "Joe", "he"),'
+    "always use the most complete identifier for that entity throughout the "
+    'knowledge graph. In this example, use "John Doe" as the entity ID.\n'
+    "Remember, the knowledge graph should be coherent and easily understandable, "
+    "so maintaining consistency in entity references is crucial.\n"
+    "## 4. Strict Compliance\n"
+    "Adhere to the rules strictly. Non-compliance will result in termination."
+)
+
+default_prompt_with_thoughts = ChatPromptTemplate.from_messages(
+    [
+        (
+            "system",
+            system_prompt,
+        ),
+        (
+            "human",
+            (
+                "Tip: Make sure to answer in the correct format and do "
+                "not include any explanations. "
+                "Use the given format to extract information from the "
+                "following input: {input}"
+            ),
+        ),
+    ]
+)
+
+def create_simple_model_with_thoughts(
+    node_labels: Optional[List[str]] = None,
+    rel_types: Optional[List[str]] = None,
+    node_properties: Union[bool, List[str]] = False,
+    llm_type: Optional[str] = None,
+    relationship_properties: Union[bool, List[str]] = False,
+) -> Type[_Graph]:
+    """
+    Create a simple graph model with optional constraints on node
+    and relationship types.
+
+    Args:
+        node_labels (Optional[List[str]]): Specifies the allowed node types.
+            Defaults to None, allowing all node types.
+        rel_types (Optional[List[str]]): Specifies the allowed relationship types.
+            Defaults to None, allowing all relationship types.
+        node_properties (Union[bool, List[str]]): Specifies if node properties should
+            be included. If a list is provided, only properties with keys in the list
+            will be included. If True, all properties are included. Defaults to False.
+        relationship_properties (Union[bool, List[str]]): Specifies if relationship
+            properties should be included. If a list is provided, only properties with
+            keys in the list will be included. If True, all properties are included.
+            Defaults to False.
+        llm_type (Optional[str]): The type of the language model. Defaults to None.
+            Only openai supports enum param: openai-chat.
+
+    Returns:
+        Type[_Graph]: A graph model with the specified constraints.
+
+    Raises:
+        ValueError: If 'id' is included in the node or relationship properties list.
+    """
+
+    node_fields: Dict[str, Tuple[Any, Any]] = {
+        "id": (
+            str,
+            Field(..., description="Name or human-readable unique identifier."),
+        ),
+        "type": (
+            str,
+            optional_enum_field(
+                node_labels,
+                description="The type or label of the node.",
+                input_type="node",
+                llm_type=llm_type,
+            ),
+        ),
+    }
+    if node_properties:
+        if isinstance(node_properties, list) and "id" in node_properties:
+            raise ValueError("The node property 'id' is reserved and cannot be used.")
+        # Map True to empty array
+        node_properties_mapped: List[str] = (
+            [] if node_properties is True else node_properties
+        )
+
+        class Property(BaseModel):
+            """A single property consisting of key and value"""
+
+            key: str = optional_enum_field(
+                node_properties_mapped,
+                description="Property key.",
+                input_type="property",
+            )
+            value: str = Field(..., description="value")
+
+        node_fields["properties"] = (
+            Optional[List[Property]],
+            Field(None, description="List of node properties"),
+        )
+    SimpleNode = create_model("SimpleNode", **node_fields)  # type: ignore
+
+    relationship_fields: Dict[str, Tuple[Any, Any]] = {
+        "source_node_id": (
+            str,
+            Field(
+                ...,
+                description="Name or human-readable unique identifier of source node",
+            ),
+        ),
+        "source_node_type": (
+            str,
+            optional_enum_field(
+                node_labels,
+                description="The type or label of the source node.",
+                input_type="node",
+            ),
+        ),
+        "target_node_id": (
+            str,
+            Field(
+                ...,
+                description="Name or human-readable unique identifier of target node",
+            ),
+        ),
+        "target_node_type": (
+            str,
+            optional_enum_field(
+                node_labels,
+                description="The type or label of the target node.",
+                input_type="node",
+            ),
+        ),
+        "type": (
+            str,
+            optional_enum_field(
+                rel_types,
+                description="The type of the relationship.",
+                input_type="relationship",
+            ),
+        ),
+    }
+    if relationship_properties:
+        if (
+            isinstance(relationship_properties, list)
+            and "id" in relationship_properties
+        ):
+            raise ValueError(
+                "The relationship property 'id' is reserved and cannot be used."
+            )
+        # Map True to empty array
+        relationship_properties_mapped: List[str] = (
+            [] if relationship_properties is True else relationship_properties
+        )
+
+        class RelationshipProperty(BaseModel):
+            """A single property consisting of key and value"""
+
+            key: str = optional_enum_field(
+                relationship_properties_mapped,
+                description="Property key.",
+                input_type="property",
+            )
+            value: str = Field(..., description="value")
+
+        relationship_fields["properties"] = (
+            Optional[List[RelationshipProperty]],
+            Field(None, description="List of relationship properties"),
+        )
+    SimpleRelationship = create_model("SimpleRelationship", **relationship_fields)  # type: ignore
+
+    class DynamicGraph(_Graph):
+        """Represents a graph document consisting of nodes and relationships."""
+        thoughts: str = Field(description="Analyze the input. Pay attention and analyze entities mentioned in it. Perhaps, you can see some interesting or popular ones? Elaborate.")
+        nodes: Optional[List[SimpleNode]] = Field(description="List of nodes")  # type: ignore
+        relationships: Optional[List[SimpleRelationship]] = Field(  # type: ignore
+            description="List of relationships"
+        )
+
+    return DynamicGraph
+
 
 def get_graph_document_list(
-    llm, combined_chunk_document_list, allowedNodes, allowedRelationship
+    llm, combined_chunk_document_list, allowed_nodes, allowed_relationship
 ):
     futures = []
     graph_document_list = []
@@ -139,24 +386,38 @@ def get_graph_document_list(
         node_properties = False
     else:
         node_properties = ["description"]
+    relationship_properties = False
     llm_transformer = LLMGraphTransformer(
         llm=llm,
         node_properties=node_properties,
-        allowed_nodes=allowedNodes,
-        allowed_relationships=allowedRelationship,
+        allowed_nodes=allowed_nodes,
+        allowed_relationships=allowed_relationship,
     )
-    with ThreadPoolExecutor(max_workers=10) as executor:
-        for chunk in combined_chunk_document_list:
-            chunk_doc = Document(
-                page_content=chunk.page_content.encode("utf-8"), metadata=chunk.metadata
+    prompt = default_prompt_with_thoughts
+    # stole this from langchain code, idk what it does
+    try:
+        llm_type = llm._llm_type
+    except Exception:
+        llm_type = None
+    schema = create_simple_model_with_thoughts(
+                allowed_nodes,
+                allowed_relationship,
+                node_properties,
+                llm_type,
+                relationship_properties,
             )
-            futures.append(
-                executor.submit(llm_transformer.convert_to_graph_documents, [chunk_doc])
-            )
-
-        for i, future in enumerate(concurrent.futures.as_completed(futures)):
-            graph_document = future.result()
-            graph_document_list.append(graph_document[0])
+    structured_llm = llm.with_structured_output(schema, include_raw=True)
+    llm_transformer.chain = prompt | structured_llm
+    for chunk in combined_chunk_document_list:
+        chunk_doc = Document(
+            page_content=chunk.page_content.encode("utf-8"), metadata=chunk.metadata
+        )
+        futures.append(
+            llm_transformer.aconvert_to_graph_documents([chunk_doc])
+        )
+    results = asyncio.gather(*futures)
+    for graph_document in results:
+        graph_document_list.append(graph_document[0])
 
     return graph_document_list
 
